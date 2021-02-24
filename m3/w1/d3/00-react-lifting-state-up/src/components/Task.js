f2f284bb94aabd012ab60e7122d1436fdd840fbc
ross-u <cirkovic.uros.srb@gmail.com>
2021-02-24 11:03:58 +0100

    Update m3 d3

--- m3/w1/d3/00-react-lifting-state-up/src/components/Task.js
+++ m3/w1/d3/00-react-lifting-state-up/src/components/Task.js
@@ -1,44 +1,54 @@
 import React from "react";
 
-class Task extends React.Component {
-  state = {
-    taskCompleted: false,
-  };
-
-  toggleTask() {
-    this.setState({ taskCompleted: !this.state.taskCompleted });
-  }
-
-  render() {
+function Task(props) {
+  const {
+    name,
+    description,
+    isDone,
+    taskId,
+    deleteTask,
+    updateTaskStatus
+  } = props;
+  
     return (
       <div className="task-card">
         <div className="task-card-half">
-          <h1>{this.props.name}</h1>
-          {this.state.taskCompleted ? (
-            <h3 style={{ color: "green" }}>DONE ✅</h3>
-          ) : (
-            <h3 style={{ color: "red" }}>PENDING</h3>
-          )}
+          <h1>{name}</h1>
+
+          {
+            isDone
+              ? <h3 style={{ color: "green" }}>DONE ✅</h3>
+              : <h3 style={{ color: "red" }}>PENDING</h3>
+          }
+
         </div>
 
         <div className="task-card-half">
           <h2>
             <u> Task Description </u>
           </h2>
-          <p>{this.props.description}</p>
+          <p>{description}</p>
+
           <button
             className="delete"
-            onClick={() => this.props.deleteTask(this.props.taskId)}
+            onClick={() => deleteTask(taskId)}
           >
             DELETE
           </button>
-          <button className="add" onClick={() => this.toggleTask()}>
-            {this.state.taskCompleted ? <span>UNDO ❌</span> : <span>✅</span>}
+
+          <button
+            className="add"
+            onClick={() => updateTaskStatus(taskId)}
+          >
+            {
+              isDone
+                ? <span>UNDO ❌</span>
+                : <span>✅</span>
+            }
           </button>
         </div>
       </div>
     );
-  }
 }
 
 export default Task;
--- m3/w1/d3/00-react-lifting-state-up/src/components/ToDoList.js
+++ m3/w1/d3/00-react-lifting-state-up/src/components/ToDoList.js
@@ -8,46 +8,73 @@ class ToDoList extends Component {
     super();
     this.state = {
       tasks: data,
+      tasksCompleted: 0
     };
 
     this.deleteTaskById = this.deleteTaskById.bind(this);
-    /*** 
-      We bind the method because it will be invoked by another component (<Task> - object)
-      Without binding, `this` will not be referring to the `<ToDoList>` component.
-      We `bind` `this` as we need it to point to <ToDoList> component (object) that holds the `state`.
-      We can as well create the method as an arrow function to solve this issue of losing context.
-    ***/
+    this.toggleTaskDone = this.toggleTaskDone.bind(this);
   }
+  /*** 
+    We bind the method because it will be invoked by another component (<Task> - object)
+    Without binding, `this` will not be referring to the `<ToDoList>` component.
+    We `bind` `this` as we need it to point to <ToDoList> component (object) that holds the `state`.
+    We can as well create the method as an arrow function to solve this issue of losing context.
+  ***/
 
   /***  
     1. State shouldn't be modified directly, therefore we use the `filter` method 
     which creates a new array
   ***/
   deleteTaskById(id) {
-    let tasksCompleted = this.state.tasksCompleted;
+    let { tasks, tasksCompleted } = this.state;
 
-    const updatedTasks = this.state.tasks.filter((taskObj, index) => {
+    const updatedTasks = tasks.filter((taskObj, index) => {
       if (taskObj.id === id) {
-        return false;
+        if (taskObj.isDone) tasksCompleted--;
+        return false; // exclude the deleted task
       }
       return true;
     });
+
     this.setState({ tasks: updatedTasks, tasksCompleted });
   }
 
+
+  toggleTaskDone (id) {
+    let { tasks, tasksCompleted } = this.state;
+
+    const tasksCopy = [...tasks]; // [{}, {}, {}, {}]
+    tasksCopy.forEach((oneTask) => {
+      if (oneTask.id === id) {
+        oneTask.isDone = oneTask.isDone ? false : true;   //    !oneTask.isDone
+
+        if (oneTask.isDone) tasksCompleted++;
+        else if (!oneTask.isDone) tasksCompleted--;
+      }
+    });
+
+    this.setState({ tasks: tasksCopy, tasksCompleted: tasksCompleted });
+  }
+
+
   render() {
+    const { tasks, tasksCompleted } = this.state;
+
     return (
       <div>
-        <Summary />
+        <Summary tasksCompleted={tasksCompleted} />
         <div className="todo-container">
-          {this.state.tasks.map((task) => {
+          
+          {tasks.map((task) => {
             return (
               <Task
                 key={task.id}
                 deleteTask={this.deleteTaskById}
+                updateTaskStatus={this.toggleTaskDone}
                 name={task.name}
                 description={task.description}
                 taskId={task.id}
+                isDone={task.isDone}
               />
             );
           })}
